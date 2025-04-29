d6c031bd29f5bd3ca0d087e3940d5f1978ff7f73
raimnascimento <raissa.mnascimento26@gmail.com>
2025-04-29 19:21:15 -0300

    implementação crud

--- scripts/crud.js
+++ scripts/crud.js
@@ -2,11 +2,24 @@ const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formularioSalvarTarefa = document.querySelector('.app__form-add-task');
 const textarea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
+const paragrafoTarefaEmAndamento = document.querySelector('.app__section-active-task-description');
+
+const btnRemoverConcluidas = document.querySelector('#btn-remover-concluidas');
+const btnRemoverTodas = document.querySelector('#btn-remover-todas');
 
 // ao inves de comecar com um array vazio, de cara já colocar local storage para guardar as informacoes desde o inicio
 // parse vai ser o contrario do stringify, pega a string e se for uma string direita (json em formato de string) ele vai conseguir transformar
 // se tiver ok, o resultado vai cair no parse. caso tenha algum valor nulo ou indefinido (sem string) vai cair no 'ou' e retornar um array vazio
-const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+let tarefaSelecionada = null;
+let liTarefaSelecionada = null;
+
+function atualizarTarefa() {
+    // toda vez que recarregamos a página, as informações são perdidas, então para guardar as informações, é utilizado localStorage
+    // localstorage.adicionaritem (quero guardar a lista inteira de tarefas)
+    // chamamos api json que tem o metodo stringify que transforma objeto em uma string
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
 
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li');
@@ -23,6 +36,17 @@ function criarElementoTarefa(tarefa) {
     const botao = document.createElement('button');
     botao.classList.add('app_button-edit');
 
+    botao.onclick = () => {
+        debugger // ajuda e entender o que está acontecendo no código
+        // precisamos abrir um prompt para o usuario digitar a nova tarefa, o js ja tem um prompt pronto
+        const novaDescricao = prompt('Qual é a nova tarefa?');
+        if(novaDescricao) {
+            paragrafo.textContent = novaDescricao;
+            tarefa.descricao = novaDescricao;
+            atualizarTarefa();
+        } 
+    }
+
     const imagem = document.createElement('img');
     imagem.setAttribute('src', '/imagens/edit.png');
 
@@ -30,6 +54,29 @@ function criarElementoTarefa(tarefa) {
     li.append(svg);
     li.append(paragrafo);
     li.append(botao);
+
+    if(tarefa.completa) {
+        li.classList.add('app__section-task-list-item-complete');
+        botao.setAttribute('disabled', 'disabled');
+    } else {
+        li.onclick = () => {
+            document.querySelectorAll('.app__section-task-list-item-active')
+                .forEach(elemento => {
+                    elemento.classList.remove('app__section-task-list-item-active');
+                })
+            if(tarefaSelecionada == tarefa) {
+                paragrafoTarefaEmAndamento.textContent = '';
+                tarefaSelecionada = null;
+                liTarefaSelecionada = null;
+                return;
+            }
+            tarefaSelecionada = tarefa;
+            liTarefaSelecionada = li;
+            paragrafoTarefaEmAndamento.textContent = tarefa.descricao;
+            li.classList.add('app__section-task-list-item-active');
+        }
+    }
+
 
     return li;
 }
@@ -49,13 +96,32 @@ formularioSalvarTarefa.addEventListener('submit', (evento) => {
     // o que for add no textarea (tarefas) serão add no array de tarefas
     tarefas.push(tarefa);
     const elementoTarefaCriado = criarElementoTarefa(tarefa);
-    // toda vez que recarregamos a página, as informações são perdidas, então para guardar as informações, é utilizado localStorage
-    // localstorage.adicionaritem (quero guardar a lista inteira de tarefas)
-    // chamamos api json que tem o metodo stringify que transforma objeto em uma string
-    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    atualizarTarefa();
 })
 
 tarefas.forEach(tarefa => {
     const elementoTarefaCriado = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefaCriado);
 });
+
+document.addEventListener('FocoFinalizado', () => {
+    if(tarefaSelecionada && liTarefaSelecionada) {
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active');
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete');
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled');
+        tarefaSelecionada.completa = true;
+        atualizarTarefa();
+    }
+})
+
+const removerTarefas = (somenteCompletas) => {
+    const seletor = somenteCompletas ? '.app__section-task-list-item-complete' : '.app__section-task-list-item';
+    document.querySelectorAll(seletor).forEach(elemento => {
+        elemento.remove();
+    })
+    tarefas = somenteCompletas ? tarefas.filter(tarefa => !tarefa.completa) : [];
+    atualizarTarefa();
+}
+
+btnRemoverConcluidas.onclick = () => removerTarefas(true);
+btnRemoverTodas.onclick = () => removerTarefas(true);
